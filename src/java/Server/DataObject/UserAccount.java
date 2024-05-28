fe09f35ddead884942902521a64d35d17bff9c82
Onairo Pérez <onairoperez48@gmail.com>
2024-05-28 13:51:26 -0500

    Lista de datos
    Se modifica el script UserAccount agregando la funcionalidad para guardar datos

--- src/java/Server/DataObject/UserAccount.java
+++ src/java/Server/DataObject/UserAccount.java
@@ -4,7 +4,14 @@
  */
 package Server.DataObject;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import java.io.IOException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
 
 /**
  *
@@ -14,8 +21,34 @@ public class UserAccount extends JsonReader<UserAccount> {
     public String email;
     public String password;
     
+    private String filePath = getPathFile("user_account.json");
+    
+    public UserAccount() {}
+    public UserAccount(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
     public UserAccount[] getData() throws IOException {
         return getData(UserAccount[].class, "user_account.json");
+    }
+    
+    public String saveData(UserAccount newData) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        
+        try {
+            List<Object> existingData = objectMapper.readValue(new File(filePath), new TypeReference<List<Object>>() {});
+            existingData.add(newData);
+
+            objectMapper.writeValue(new File(filePath), existingData);
+            
+        } catch (IOException e) {
+           try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+                writer.write("}]");
+                return "done";
+            }
+        }
+        return null;
     }
 }
 
